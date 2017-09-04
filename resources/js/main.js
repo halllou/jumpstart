d99314060962054ddb967388d50796bf88bf3075
halllou <halllou1987@gmail.com>
2017-09-04 21:40:20 +0100

    Fourth commit.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,3 @@
-/* Slick */
-
 $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
@@ -10,20 +8,17 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
-});
 
-/* Nav hide */
-
-var lastScrollTop = 0;
-$(window).scroll(function(){
-  var scrollTop = $(this).scrollTop();
-  if (scrollTop - lastScrollTop > 50){
-    var navHeight = $('.navbar').css('height');
-    $('.navbar').animate({top: '-' + navHeight}, 150);
-    lastScrollTop = scrollTop;
-  } else if (lastScrollTop - scrollTop > 50) {
-    $('.navbar').animate({top: '0px'}, 150);
-    lastScrollTop = scrollTop;
-  }
-});
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
 });
